36c268975b53a200fe2e3b8ee38ae8e26dcd31dc
nolli <nolliepolieollie@gmail.com>
2023-07-31 18:04:05 -0700

    first commit

---
+++ README/README.txt
@@ -0,0 +1,27 @@
+# <Note Taker>
+
+## A place to write, keep and delete notes. 
+
+- Building a working app with starter code only for front end. I did all the back end, and connections. 
+- More building within a CLI, and getting comfortable starting and using local servers. 
+- A simple place to keep notes handy. I think of it as a "TO DO:" list. 
+- I learned that it's really annoying to have to restart the server everytime to want to run the app. 
+
+## Installation
+
+Follow link provided. 
+
+## Usage
+- click "+" to add a new note. 
+- click "save" (floppy disk icon next to "+") when finished. 
+- when task is completed, click "delete" (trash icon)
+
+## Credits
+
+Main author: Nicole Smith. 
+
+## License
+
+MIT
+
+---
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
+const fs = require('fs')
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+//create
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')))
+
+  res.json(notes)
+
+})
+
+// post 
+app.post('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')))
+  const newNote = req.body; 
+  newNote.id = parseInt(Math.random() * 1000);
+ //const newNote = { title: req.body.title, text: req.body.text, id: parseInt(Math.random() * 1000) };
+  console.log ("new note: "+JSON.stringify( newNote))
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes))
+  res.json(newNote)
+})
+
+// delete 
+app.delete('/api/notes/:id', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')))
+  const idToDelete = req.params.id;
+  console.log("Id they want is "+idToDelete);
+  const noteIndex = notes.findIndex((note) => note.id == idToDelete);
+
+  if (noteIndex === -1) {
+    return res.status(404).json({ error: 'error' });
+  }
+
+  notes.splice(noteIndex, 1);
+
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
+  res.json({ message: 'note deleted successfully' });
+})
+
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '../public/index.html')) })
+app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname, '../public/notes.html')) })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
